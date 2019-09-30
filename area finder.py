d6b3696be3be403710e3a77e14332cec5d643c48
DumDumGiveMeGumGum <55249948+DumDumGiveMeGumGum@users.noreply.github.com>
2019-09-30 10:14:30 -0700

    Update area finder.py

--- area finder.py
+++ area finder.py
@@ -1,6 +1,6 @@
-L1= input ("What is your length: ")
-H1= input ("what is your height: ")
+L1= input ("What is your length: ")             #length Input
+H1= input ("what is your height: ")             #hight Input
 
-LH1 = int(L1)*int(H1)
+LH1 = int(L1)*int(H1)                             #length times hight
 print ("your area is")
 print (LH1)
